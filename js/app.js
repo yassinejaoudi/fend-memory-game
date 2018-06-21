50f443b64421a565acf33f0f4ca248e54da0f21e
yassinejaoudi <jaoudiyassine@gmail.com>
2018-06-21 16:14:36 -0400

    Add display card

--- js/app.js
+++ js/app.js
@@ -48,3 +48,11 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ var displayCard = function(){
+     this.classList.toggle('show');
+     this.classList.toggle('open');
+     this.classList.toggle('disabled');
+ }
+
+ 
